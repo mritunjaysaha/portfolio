e3e67e2e7f2154254f36309a2d1eac766a181350
mritunjaysaha <59063979+mritunjaysaha@users.noreply.github.com>
2020-10-08 18:25:38 +0530

    added all the pages

---
+++ src/components/navbar.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export function Navbar() {
+    return (
+        <nav>
+            <div className="logo">
+                <p>@Mritunjay_Saha_</p>
+            </div>
+            <div className="contents">
+                <p>Projects</p>
+                <p>Blogs</p>
+                <p>About</p>
+                <p>Contacts</p>
+            </div>
+        </nav>
+    );
+}
