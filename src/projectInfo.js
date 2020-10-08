404b3cdeeceb901ef1cae3ec3f252b7c4161685c
mritunjaysaha <59063979+mritunjaysaha@users.noreply.github.com>
2020-10-08 14:19:03 +0530

    added project details

---
+++ src/projectInfo.js
@@ -0,0 +1,57 @@
+export const projects = [
+    {
+        id: 1,
+        name: "Covid-19: India Stats",
+        description:
+            "A vanillaJS application to track the number of Covid-19 cases in India with the number of increase cases for each state and for each parameter.",
+        repo: "https://github.com/mritunjaysaha/coronavirus-tracker",
+        demo: "https://covidupdate.netlify.app/",
+        stack: ["JavaScript", "HTML", "CSS", "Chart.js", "Bootstrap"],
+    },
+    {
+        id: 2,
+        name: "Markdown Previewer",
+        description:
+            "A React.js application that can be used to learn markdown. It has tools to help understand the use of markdown.",
+        repo: "https://github.com/mritunjaysaha/markdown-previewer",
+        demo: "https://mritunjay-markdown-previewer.netlify.app/",
+        stack: ["React.js", "Material-UI", "Sass"],
+    },
+    {
+        id: 3,
+        name: "Air Quality Monitor",
+        description:
+            "A vanillaJS web application that allows the user to see the air quality index of multiple cities at a time. User doesn’t need to enter the details of the cities Everytime the page reloads. The data is saved on the browser.",
+        repo: "https://github.com/mritunjaysaha/air-quality-monitor",
+        demo: "https://aqimonitor.netlify.app/",
+        stack: ["JavaScript", "HTML", "CSS"],
+    },
+    {
+        id: 4,
+        name: "TodoTimer PWA",
+        description:
+            "Most of the time we create a todo without any deadline and often forget about it. This Progressive Web Application helps you to create todos with a deadline.",
+        repo: "https://github.com/mritunjaysaha/todo-pwa",
+        demo: "https://todotimer.netlify.app/",
+        stack: ["React.js", "Ant Design", "Sass", "Popup.js"],
+    },
+    {
+        id: 5,
+        name: "Rock Paper Scissors",
+        description: "A rock-paper-scissor game made using VanillaJS.",
+        repo:
+            "https://github.com/mritunjaysaha/rest-countries-api-with-color-switching",
+        demo: "https://rest-countries-api-with-color-switching.vercel.app/",
+        stack: ["JavaScript", "HTML", "CSS"],
+    },
+    {
+        id: 6,
+        name: "Where in the world?",
+        description:
+            "A ReactJS application that shows the list of countries and their details. If the network is slow then it won't feel buggy as I have implemented loading-skeleton-placeholder.",
+        repo:
+            "https://github.com/mritunjaysaha/rest-countries-api-with-color-switching",
+        demo: "https://rest-countries-api-with-color-switching.vercel.app/",
+        stack: ["React.js", "Material-UI", "React-reveal", "Sass"],
+    },
+];
