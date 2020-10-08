dda796964558e97018172afd2bd2326678db0746
mritunjaysaha <59063979+mritunjaysaha@users.noreply.github.com>
2020-10-08 19:05:30 +0530

    added navbar and links

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 import Head from "next/head";
+import { Navbar } from "../src/components/navbar";
 
 export default function Home() {
-    return <>Index</>;
+    return (
+        <>
+            <Navbar />
+        </>
+    );
 }
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,16 +1,24 @@
 import React from "react";
+import Link from "next/link";
+import styles from "../../styles/pages/Home.module.scss";
 
 export function Navbar() {
     return (
-        <nav>
-            <div className="logo">
+        <nav className={styles.navbar}>
+            <div className={styles.logo}>
                 <p>@Mritunjay_Saha_</p>
             </div>
-            <div className="contents">
-                <p>Projects</p>
-                <p>Blogs</p>
-                <p>About</p>
-                <p>Contacts</p>
+            <div className={styles.links}>
+                <Link href="/projects">
+                    <a>Projects</a>
+                </Link>
+                <Link href="/blogs">
+                    <a>Blogs</a>
+                </Link>
+                <Link href="/about">
+                    <a>About</a>
+                </Link>
+                <a href="">Contacts</a>
             </div>
         </nav>
     );
