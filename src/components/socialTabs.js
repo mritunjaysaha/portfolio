04710e4c8803d422f4db64ed3173c1fc62f16a65
mritunjaysaha <59063979+mritunjaysaha@users.noreply.github.com>
2020-10-09 00:07:41 +0530

    added side panel

---
+++ src/components/socialTabs.js
@@ -0,0 +1,15 @@
+import React from "react";
+import styles from "../../styles/pages/Home.module.scss";
+
+export function SocialTabs() {
+    return (
+        <aside className={styles["social-tabs"]}>
+            <ul>
+                <li>a</li>
+                <li>a</li>
+                <li>a</li>
+                <li>a</li>
+            </ul>
+        </aside>
+    );
+}
