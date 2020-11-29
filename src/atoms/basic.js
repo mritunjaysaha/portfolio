fa1e573f7ab9ce04a7ef52fe50e5d0e0b80ade0c
mritunjaysaha <59063979+mritunjaysaha@users.noreply.github.com>
2020-11-29 14:16:40 +0530

    added social media icons

---
+++ src/atoms/basic.js
@@ -0,0 +1,3 @@
+export default {
+    color: "#16161d",
+};
--- src/components/socialTabs.js
+++ src/components/socialTabs.js
@@ -1,6 +1,27 @@
 import React from "react";
 /**@jsx jsx */
 import { css, jsx } from "@emotion/core";
+import Link from "next/link";
+import { UilLinkedin, UilGithub, UilTwitter } from "@iconscout/react-unicons";
+
+function Anchor(props) {
+    return (
+        <Link href={props.link || "#"}>
+            <a
+                css={css`
+                    color: ${props.color || "#000"};
+                `}
+            >
+                {props.icon}
+            </a>
+        </Link>
+    );
+}
+
+const socialIcons = css`
+    height: 3rem;
+    width: 3rem;
+`;
 
 export function SocialTabs() {
     return (
@@ -20,11 +41,46 @@ export function SocialTabs() {
                 }
             `}
         >
-            <ul>
-                <li>a</li>
-                <li>a</li>
-                <li>a</li>
-                <li>a</li>
+            <ul
+                css={css`
+                    display: flex;
+                    flex-direction: column;
+                    justify-content: space-between;
+                    & > li {
+                        margin: 1rem 0;
+                    }
+                `}
+            >
+                <li>
+                    <Anchor
+                        icon={<UilGithub css={socialIcons} />}
+                        link="https://github.com/mritunjaysaha"
+                    />
+                </li>
+                <li>
+                    <Anchor
+                        icon={<UilTwitter css={socialIcons} />}
+                        link="https://github.com/mritunjaysaha"
+                    />
+                </li>
+                <li>
+                    <Anchor
+                        icon={<UilLinkedin css={socialIcons} />}
+                        link="https://github.com/mritunjaysaha"
+                    />
+                </li>
+                <li>
+                    <Anchor
+                        icon={
+                            <img
+                                src="https://d2fltix0v2e0sb.cloudfront.net/dev-badge.svg"
+                                alt="Mritunjay Saha's DEV Profile"
+                                css={socialIcons}
+                            />
+                        }
+                        link="https://dev.to/mritunjaysaha"
+                    />
+                </li>
             </ul>
         </aside>
     );
